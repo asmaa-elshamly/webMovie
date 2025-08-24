32632231229addda58f303b413bb465e2e7884b3
Asmaa Elshamly <asmaaelshamly85@gmail.com>
2025-08-24 13:57:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//   part-1
+  const myCarousel = document.querySelector('#carouselExampleCaptions');
+  const carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 3000,
+    ride: 'carousel',
+    pause: false // mouse
+  });
+
+
+// end part-1
